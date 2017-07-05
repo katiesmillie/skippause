500cc80dc22945d695cd4d4ad901f4a34dc5a413
Katie Smillie <katiesmillie@gmail.com>
2017-07-05 13:24:47 -0400

    Trying getting samples with AudioBufferList

--- SkipPause/AssetReader.swift
+++ SkipPause/AssetReader.swift
@@ -29,7 +29,7 @@ class AssetReader: Operation {
     var samplesWithTimes: [(CGFloat, CMTime)] = []
     var playerItem: AVPlayerItem?
     
-    var decibelThreshold: CGFloat = 40
+    var decibelThreshold: CGFloat = 60
     var sampleDuration: Double?
     
     init(soundFile: SoundFile, completionHandler: @escaping (_ item: AVPlayerItem?) -> ()) {
@@ -71,19 +71,19 @@ class AssetReader: Operation {
     private func render() {
         guard let url = soundFile.playbackURL else { return }
         let asset = AVAsset(url: url)
-        fetchSamples(from: asset)
+        fetchSamples2(from: asset)
         let item = createComposition(asset: asset)
         finish(with: item)
     }
     
     func createComposition(asset: AVAsset) -> AVPlayerItem {
         
-        // Try calculating sampleDuration by the difference between consecutive samples
-        // vs the formula from the documentation
-        if presentationTimes.count > 1 {
-            sampleDuration = presentationTimes[1].seconds - presentationTimes[0].seconds
-        }
-        
+//        // Try calculating sampleDuration by the difference between consecutive samples
+//        // vs the formula from the documentation
+//        if presentationTimes.count > 1 {
+//            sampleDuration = presentationTimes[1].seconds - presentationTimes[0].seconds
+//        }
+//        
         
         let mutableComposition = AVMutableComposition()
         
@@ -113,6 +113,130 @@ class AssetReader: Operation {
         }
         print(newTrack.asset!.duration.seconds)
         return AVPlayerItem(asset: mutableComposition)
+    }
+    
+    func createComposition2(asset: AVAsset) -> AVPlayerItem {
+        
+        let mutableComposition = AVMutableComposition()
+        
+        let type = AVMediaTypeAudio
+        let prefTrackID = kCMPersistentTrackID_Invalid
+        
+        let sourceTrack: AVAssetTrack = asset.tracks(withMediaType: type).first!
+        let newTrack = mutableComposition.addMutableTrack(withMediaType: type, preferredTrackID: prefTrackID)
+        print(newTrack.asset!.duration.seconds)
+        
+        do {
+            let startTime = kCMTimeZero
+            let duration = asset.duration
+            let range = CMTimeRangeMake(startTime, duration)
+            try newTrack.insertTimeRange(range, of: sourceTrack, at: startTime)
+            print(newTrack.asset!.duration.seconds)
+        } catch { print(error) }
+        
+        print(presentationTimes.count)
+        presentationTimes.forEach { time in
+            
+            let startTime = time
+            let duration = CMTime(seconds: sampleDuration!, preferredTimescale: 600)
+            let range = CMTimeRangeMake(startTime, duration)
+            newTrack.removeTimeRange(range)
+            
+        }
+        print(newTrack.asset!.duration.seconds)
+        return AVPlayerItem(asset: mutableComposition)
+    }
+    
+    
+    func fetchSamples2(from asset: AVAsset) {
+        
+        guard !isCancelled else { return }
+        
+        guard
+            let track = asset.tracks(withMediaType: AVMediaTypeAudio).first,
+            let reader = try? AVAssetReader(asset: asset)
+            else { return }
+        
+        let outputSettingsDict: [String: Any] = [
+            AVFormatIDKey: Int(kAudioFormatLinearPCM),
+            AVLinearPCMBitDepthKey: 16,
+            AVLinearPCMIsBigEndianKey: false,
+            AVLinearPCMIsFloatKey: false,
+            AVLinearPCMIsNonInterleaved: false
+        ]
+        
+        let readerOutput = AVAssetReaderTrackOutput(track: track, outputSettings: outputSettingsDict)
+        readerOutput.alwaysCopiesSampleData = false
+        reader.add(readerOutput)
+        
+        var channelCount = 1
+        var sampleRate = 0.0
+        var framesPerPacket: Float64 = 0
+        
+        let formatDescriptions = track.formatDescriptions as! [CMAudioFormatDescription]
+        for item in formatDescriptions {
+            guard let formatDescription = CMAudioFormatDescriptionGetStreamBasicDescription(item) else { return }
+            sampleRate = formatDescription.pointee.mSampleRate // 44100
+            channelCount = Int(formatDescription.pointee.mChannelsPerFrame)
+            framesPerPacket = Float64(formatDescription.pointee.mFramesPerPacket) // 1152
+        }
+        
+        sampleDuration = (1 / sampleRate) * framesPerPacket
+        
+        var adjustedRate: Int = Int(sampleRate)
+        
+        let filter = [Float](repeating: 1.0 / Float(adjustedRate), count: adjustedRate)
+        
+        var outputSamples = [CGFloat]()
+        var sampleBuffer = Data()
+        
+        var sampleData = Data()
+        
+        // 16-bit samples
+        reader.startReading()
+        defer { reader.cancelReading() } // Cancel reading if we exit early if operation is cancelled
+        
+        while reader.status == .reading {
+            guard !isCancelled else { return }
+            
+            guard let readSampleBuffer = readerOutput.copyNextSampleBuffer(),
+                let readBuffer = CMSampleBufferGetDataBuffer(readSampleBuffer) else { break }
+            
+            
+            var audioBufferList = AudioBufferList(mNumberBuffers: 1, mBuffers: AudioBuffer(mNumberChannels: UInt32(channelCount), mDataByteSize: 0, mData: nil))
+            
+            var blockBuffer: CMBlockBuffer? = nil
+            
+            CMSampleBufferGetAudioBufferListWithRetainedBlockBuffer(readSampleBuffer, nil, &audioBufferList, MemoryLayout<AudioBufferList>.size, nil, nil, UInt32(kCMSampleBufferFlag_AudioBufferList_Assure16ByteAlignment), &blockBuffer)
+       
+            let presentationTime: CMTime? = CMSampleBufferGetPresentationTimeStamp(readSampleBuffer)
+            print(presentationTime?.seconds)
+
+            var sampleOutput: [Int16] = []
+            
+            let audioBufferListPointer = UnsafeMutableAudioBufferListPointer(&audioBufferList)
+            
+            audioBufferListPointer.forEach { buffer in
+                let samples = UnsafeMutableBufferPointer<Int16>(start: UnsafeMutablePointer(OpaquePointer(buffer.mData)),
+                                                                count: Int(buffer.mDataByteSize)/MemoryLayout<Int16>.size)
+                
+                samples.forEach { sample in
+                    sampleOutput += [sample]
+                }
+                
+            }
+            
+            let max = Int(sampleOutput.max()!)
+            print(max)
+            print(abs(max))
+            
+            
+            if decibel(CGFloat(max)) < decibelThreshold {
+                presentationTimes += [presentationTime!]
+            }
+            
+        }
+        
     }
     
     // Read the asset and create create a lower resolution set of samples
@@ -179,11 +303,11 @@ class AssetReader: Operation {
             let samples = data?.mutableBytes.assumingMemoryBound(to: UInt8.self)
             sampleData.append(samples!, count: length)
             let presentationTime: CMTime? = CMSampleBufferGetPresentationTimeStamp(readSampleBuffer)
-
+            
             CMSampleBufferInvalidate(readSampleBuffer)
             
             print("episode: \(soundFile.resource) -- sample data \(sampleData.count) -- presentation time \(presentationTime!.seconds)")
-
+            
             
             // Using CMBlockBufferGetDataPointer
             var readBufferLength = 0
@@ -191,19 +315,19 @@ class AssetReader: Operation {
             CMBlockBufferGetDataPointer(readBuffer, 0, &readBufferLength, nil, &readBufferPointer)
             
             sampleBuffer.append(UnsafeBufferPointer(start: readBufferPointer, count: readBufferLength))
-//            let presentationTime: CMTime? = CMSampleBufferGetPresentationTimeStamp(readSampleBuffer)
+            //            let presentationTime: CMTime? = CMSampleBufferGetPresentationTimeStamp(readSampleBuffer)
             CMSampleBufferInvalidate(readSampleBuffer)
             
             print("episode: \(soundFile.resource) -- sample buffer \(sampleBuffer.count) -- presentation time \(presentationTime!.seconds)")
             
-
+            
             
             // Use sampleBuffer or sampleData
             let totalSamples = sampleData.count / MemoryLayout<UInt8>.size
             
             let downSampledLength = totalSamples / adjustedRate
-//            let samplesToProcess = downSampledLength * adjustedRate
-//            guard samplesToProcess > 0 else { continue }
+            //            let samplesToProcess = downSampledLength * adjustedRate
+            //            guard samplesToProcess > 0 else { continue }
             
             // Pass in totalSamples for samplesToProcess to process when NOT downsampling
             let samplesToProcess = totalSamples
@@ -219,7 +343,7 @@ class AssetReader: Operation {
         
         if reader.status == .completed {
             
-//            normalize()
+            //            normalize()
         }
         
         if reader.status == .failed {
@@ -247,7 +371,7 @@ class AssetReader: Operation {
         
         presentationTimes = normalizedData.filter { $0.0 < filterThreshold }.map { $0.1 }
         
-  
+        
     }
     
     func processSamples(fromData sampleBuffer: inout Data, outputSamples: inout [CGFloat], samplesToProcess: Int, downSampledLength: Int, adjustedRate: Int, filter: [Float], presentationTime: CMTime?) {
@@ -265,14 +389,14 @@ class AssetReader: Operation {
             vDSP_vabs(processingBuffer, 1, &processingBuffer, 1, sampleCount)
             
             //Downsample and average
-//            var downSampledData = [Float](repeating: 0.0, count: downSampledLength)
-//            vDSP_desamp(processingBuffer,
-//                        vDSP_Stride(adjustedRate),
-//                        filter,
-//                        &downSampledData,
-//                        vDSP_Length(downSampledLength),
-//                        vDSP_Length(adjustedRate))
-//            
+            //            var downSampledData = [Float](repeating: 0.0, count: downSampledLength)
+            //            vDSP_desamp(processingBuffer,
+            //                        vDSP_Stride(adjustedRate),
+            //                        filter,
+            //                        &downSampledData,
+            //                        vDSP_Length(downSampledLength),
+            //                        vDSP_Length(adjustedRate))
+            //
             
             // Use downSampledData instead of processingBuffer when downsampling
             let downSampledDataCG = processingBuffer.map { CGFloat($0) }
@@ -290,7 +414,7 @@ class AssetReader: Operation {
                 presentationTimes += [presentationTime!]
             }
             
-
+            
         }
     }
     
