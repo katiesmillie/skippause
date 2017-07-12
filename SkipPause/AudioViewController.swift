a823c10d198127a4cf8f26d578238d65fda4e644
Katie Smillie <katiesmillie@gmail.com>
2017-07-12 12:50:37 -0400

    Remove clipped player and AssetReader so it’s just an example for skipping silences with AVAudioPlayer

--- SkipPause/AudioViewController.swift
+++ SkipPause/AudioViewController.swift
@@ -11,237 +11,226 @@ import AVFoundation
 
 enum TypePlaying {
     case original
-    case clipped
-    case audioPlayer
+    case skipped
 }
 
 class AudioViewController: UIViewController {
     
-    var originalPlayer: AVPlayer?
-    var clippedPlayer: AVPlayer?
-    var audioPlayer: AVAudioPlayer?
-    
-    var reader: AssetReader?
+    var originalPlayer: AVAudioPlayer?
+    var skippedPlayer: AVAudioPlayer?
     
     var timer: Timer?
     var audioPlayerTimer: Timer?
     
-    var increasedPlayback: Int = 0
+    var secondsOfIncreasedPlayback = 0.0
     
     var typePlaying: TypePlaying = .original
     
     var nowPlaying: SoundFile?
     
-    
-    let freakonomics = SoundFile(resource: "freakonomics")
-    let notetoself = SoundFile(resource: "notetoself")
     let piano = SoundFile(resource: "piano")
-    let brass = SoundFile(resource: "brass")
-    let empty = SoundFile(resource: "empty")
+    let replyAll = SoundFile(resource: "replyall")
+    let strangers = SoundFile(resource: "strangers")
     
+    let playImage = UIImage(named: "Audio_Bar_Play")
+    let pauseImage = UIImage(named: "Audio_Bar_Pause")
+    
+    @IBOutlet weak var picker: UIPickerView?
     @IBOutlet weak var segementedControl: UISegmentedControl?
     
-    @IBOutlet weak var originalSeconds: UILabel?
-    @IBOutlet weak var clippedSeconds: UILabel?
-    @IBOutlet weak var audioPlayerSeconds: UILabel?
+    @IBOutlet weak var originalTimestamp: UILabel?
+    @IBOutlet weak var skippedTimestamp: UILabel?
     
-    @IBOutlet weak var originalTotalSeconds: UILabel?
-    @IBOutlet weak var clippedTotalSeconds: UILabel?
+    @IBOutlet weak var originalSeconds: UILabel?
     @IBOutlet weak var secondsSaved: UILabel?
     
     @IBOutlet weak var originalSlider: UISlider?
-    @IBOutlet weak var clippedSlider: UISlider?
-    @IBOutlet weak var audioPlayerSlider: UISlider?
-
-
+    @IBOutlet weak var skippedSlider: UISlider?
+    
+    @IBOutlet weak var originalPlayPauseButton: UIButton?
+    @IBOutlet weak var skippedPlayPauseButton: UIButton?
+    
     override func viewDidLoad() {
         nowPlaying = piano
-        clearLabels()
-        setUpOriginal()
-        
-    }
-    
-    func setUpOriginal(){
-        guard let nowPlaying = nowPlaying, let url = nowPlaying.playbackURL else {return }
-        let playerItem = AVPlayerItem(url: url)
-        originalPlayer = AVPlayer(playerItem: playerItem)
-        self.originalTotalSeconds?.text = "\((playerItem.asset.duration.seconds).roundTo(places: 2)) seconds"
-    }
-
-    func updateRate() {
-        guard audioPlayer?.isPlaying == true else {
-            audioPlayerTimer!.invalidate()
-            return
-        }
-        let decibelThreshold = Float(-35)
-        
-        audioPlayer?.updateMeters()
-        
-        let averagePower = audioPlayer?.averagePower(forChannel: 1)
-        let peakPower = audioPlayer?.peakPower(forChannel: 1)
-        
-        if averagePower! < decibelThreshold {
-            audioPlayer?.rate = 3
-            increasedPlayback += 1
-            let timeAtFasterRate = Double(increasedPlayback) * 0.1
-            let seconds = (timeAtFasterRate - (timeAtFasterRate / 3)).roundTo(places: 2)
-            print("seconds saved: \(seconds)")
-            secondsSaved?.text = ("\(seconds) seconds saved")
-        } else {
-            audioPlayer?.rate = 1
-        }
+        reset()
     }
     
     @IBAction func changedValue(_ sender: UISegmentedControl) {
-        clearLabels()
-        self.originalTotalSeconds?.text = ""
-
+        reset()
+        originalSeconds?.text = ""
+        
         if sender.selectedSegmentIndex == 0 {
             nowPlaying = piano
         } else if sender.selectedSegmentIndex == 1 {
-            nowPlaying = brass
+            nowPlaying = replyAll
         } else if sender.selectedSegmentIndex == 2 {
-            nowPlaying = freakonomics
-        } else if sender.selectedSegmentIndex == 3 {
-            nowPlaying = notetoself
-        } else if sender.selectedSegmentIndex == 4 {
-            nowPlaying = empty
+            nowPlaying = strangers
         }
-        setUpOriginal()
     }
     
-    func clearLabels() {
-        originalPlayer?.pause()
-        clippedPlayer?.pause()
-        audioPlayer?.pause()
+    func reset() {
+        originalPlayer = nil
+        skippedPlayer = nil
         
-        clippedTotalSeconds?.text = ""
-        secondsSaved?.text = ""
+        originalPlayPauseButton?.setImage(playImage, for: .normal)
+        skippedPlayPauseButton?.setImage(playImage, for: .normal)
+
+        secondsSaved?.text = "0"
         
         audioPlayerTimer?.invalidate()
         timer?.invalidate()
         
-        clippedSlider?.setValue(0, animated: true)
         originalSlider?.setValue(0, animated: true)
-        audioPlayerSlider?.setValue(0, animated: true)
+        skippedSlider?.setValue(0, animated: true)
         
-        audioPlayerSeconds?.text = 0.0.timeString()
-        originalSeconds?.text = 0.0.timeString()
-        clippedSeconds?.text = 0.0.timeString()
+        originalTimestamp?.text = 0.0.timeString()
+        skippedTimestamp?.text = 0.0.timeString()
         
-        increasedPlayback = 0
+        secondsOfIncreasedPlayback = 0
     }
     
-    @IBAction func tappedPlayeAudio(_ sender: UIButton) {
-        originalPlayer?.pause()
-        clippedPlayer?.pause()
-        
-        guard let nowPlaying = nowPlaying, let url = nowPlaying.playbackURL else {return }
-        let playerItem = AVPlayerItem(url: url)
-        let duration = playerItem.asset.duration.seconds
-        
-        self.audioPlayerSlider?.maximumValue = Float(duration)
-        self.audioPlayerSlider?.minimumValue = 0.0
-        
-        audioPlayer = try? AVAudioPlayer(contentsOf: url)
-        audioPlayer?.isMeteringEnabled = true
-        audioPlayer?.enableRate = true
-        
-        activateSmartSpeed()
-        
-        typePlaying = .audioPlayer
-        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateSlider), userInfo: nil, repeats: true)
-    }
-    
-    func activateSmartSpeed() {
-        audioPlayer?.play()
-        audioPlayerTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateRate), userInfo: nil, repeats: true)
-    }
-    
-    @IBAction func tappedPlayOriginal(_ sender: UIButton) {
-        audioPlayer?.pause()
-        clippedPlayer?.pause()
-        
-        guard let nowPlaying = nowPlaying, let url = nowPlaying.playbackURL else {return }
-        let playerItem = AVPlayerItem(url: url)
-        originalPlayer = AVPlayer(playerItem: playerItem)
-        self.originalTotalSeconds?.text = "\((playerItem.asset.duration.seconds).roundTo(places: 2)) seconds"
-        
-        let duration = playerItem.asset.duration.seconds
-        originalSlider?.maximumValue = Float(duration)
-        originalSlider?.minimumValue = 0.0
-        originalPlayer?.play()
-        
-        originalPlayer?.play()
+    @IBAction func playOriginalAudio(_ sender: UIButton) {
+        skippedPlayer?.pause()
+        skippedPlayPauseButton?.setImage(playImage, for: .normal)
 
+        if let originalPlayer = originalPlayer {
+            if originalPlayer.isPlaying {
+                originalPlayer.pause()
+                originalPlayPauseButton?.setImage(playImage, for: .normal)
+            } else {
+                originalPlayer.play()
+                originalPlayPauseButton?.setImage(pauseImage, for: .normal)
+            }
+        } else {
+            startOriginalAudio()
+        }
+    }
+
+    func startOriginalAudio() {
+        guard let nowPlaying = nowPlaying, let url = nowPlaying.playbackURL else {return }
         
+        originalPlayer = try? AVAudioPlayer(contentsOf: url)
+        originalPlayer?.delegate = self
+        originalPlayer?.isMeteringEnabled = true
+        originalPlayer?.enableRate = true
+        
+        if let duration = originalPlayer?.duration {
+            originalSlider?.maximumValue = Float(duration)
+            originalSlider?.minimumValue = 0.0
+        }
+        originalPlayer?.play()
+        originalPlayPauseButton?.setImage(pauseImage, for: .normal)
+
         typePlaying = .original
         timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateSlider), userInfo: nil, repeats: true)
     }
     
-    @IBAction func tappedPlayClipped(_ sender: UIButton) {
-        audioPlayer?.pause()
+    @IBAction func playSkippedAudio(_ sender: UIButton) {
         originalPlayer?.pause()
-        clippedTotalSeconds?.text = "processing..."
-        
-        let typePlaying = self.typePlaying
-        
-        reader = AssetReader(soundFile: nowPlaying!) { playerItem in
-            DispatchQueue.main.async {
-                
-                guard typePlaying == self.typePlaying else { return }
-                guard let playerItem = playerItem else { return }
-                self.clippedPlayer = AVPlayer(playerItem: playerItem)
-                
-                let duration = playerItem.asset.duration.seconds
-                self.clippedSlider?.maximumValue = Float(duration)
-                self.clippedSlider?.minimumValue = 0.0
-                self.clippedTotalSeconds?.text = "\((playerItem.asset.duration.seconds).roundTo(places: 2)) seconds"
-                
-                self.clippedPlayer?.play()
-                self.typePlaying = .clipped
-                self.timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(self.updateSlider), userInfo: nil, repeats: true)
+        originalPlayPauseButton?.setImage(playImage, for: .normal)
+
+        if let skippedPlayer = skippedPlayer {
+            if skippedPlayer.isPlaying {
+                skippedPlayer.pause()
+                skippedPlayPauseButton?.setImage(playImage, for: .normal)
+            } else {
+                skippedPlayer.play()
+                skippedPlayPauseButton?.setImage(pauseImage, for: .normal)
             }
+        } else {
+            startSkippedAudio()
         }
-        reader?.start()
+    }
+    
+    func startSkippedAudio() {
+        guard let nowPlaying = nowPlaying, let url = nowPlaying.playbackURL else {return }
+        
+        skippedPlayer = try? AVAudioPlayer(contentsOf: url)
+        skippedPlayer?.delegate = self
+        skippedPlayer?.isMeteringEnabled = true
+        skippedPlayer?.enableRate = true
+        
+        if let duration = skippedPlayer?.duration {
+            skippedSlider?.maximumValue = Float(duration)
+            skippedSlider?.minimumValue = 0.0
+        }
+        
+        skippedPlayer?.play()
+        skippedPlayPauseButton?.setImage(pauseImage, for: .normal)
+
+        skipSilences()
+        
+        typePlaying = .skipped
+        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateSlider), userInfo: nil, repeats: true)
     }
     
     @IBAction func scrubbedOriginalSlider(_ sender: UISlider) {
-        let time = CMTime(seconds: Double(sender.value), preferredTimescale: 600)
-        originalPlayer?.seek(to: time)
-        originalSeconds?.text = time.seconds.timeString()
+        originalPlayer?.currentTime = Double(sender.value)
+        originalTimestamp?.text = Double(sender.value).timeString()
     }
     
-    @IBAction func scrubbedClippedSlider(_ sender: UISlider) {
-        let time = CMTime(seconds: Double(sender.value), preferredTimescale: 600)
-        clippedPlayer?.seek(to: time)
-        clippedSeconds?.text = time.seconds.timeString()
+    @IBAction func scrubbedSkippedSlider(_ sender: UISlider) {
+        skippedPlayer?.currentTime = Double(sender.value)
+        skippedTimestamp?.text = Double(sender.value).timeString()
+        skipSilences()
     }
     
-    @IBAction func scrubbedAudioSlider(_ sender: UISlider) {
-        audioPlayer?.currentTime = Double(sender.value)
-        audioPlayerSeconds?.text = Double(sender.value).timeString()
-        clearLabels()
-        activateSmartSpeed()
+    func skipSilences() {
+        skippedPlayer?.play()
+        audioPlayerTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateRate), userInfo: nil, repeats: true)
     }
     
-    func updateSlider() {
-        switch typePlaying{
-        case .clipped:
-            guard let seconds = clippedPlayer?.currentTime().seconds else { return }
-            clippedSlider?.setValue(Float(seconds), animated: true)
-            self.clippedSeconds?.text = "\(seconds.timeString())"
-        case .original:
-            guard let seconds = originalPlayer?.currentTime().seconds else { return }
-            originalSlider?.setValue(Float(seconds), animated: true)
-            self.originalSeconds?.text = "\(seconds.timeString())"
-        case .audioPlayer:
-            guard let seconds = audioPlayer?.currentTime else { return }
-            audioPlayerSlider?.setValue(Float(seconds), animated: true)
-            self.audioPlayerSeconds?.text = "\(seconds.timeString())"
+    func updateRate() {
+        guard skippedPlayer?.isPlaying == true else { return }
+        let decibelThreshold = Float(-35)
+        
+        skippedPlayer?.updateMeters()
+                
+        if let averagePower = skippedPlayer?.averagePower(forChannel: 1),
+            averagePower < decibelThreshold {
+            skippedPlayer?.rate = 3
+            secondsOfIncreasedPlayback += 0.1
+            let totalSecondsSaved = (secondsOfIncreasedPlayback / 3).roundTo(places: 2)
+            print("seconds saved: \(totalSecondsSaved)")
+            secondsSaved?.text = ("\(totalSecondsSaved)")
+        } else {
+            skippedPlayer?.rate = 1
         }
     }
     
+    func updateSlider() {
+        switch typePlaying {
+        case .original:
+            guard let seconds = originalPlayer?.currentTime else { return }
+            originalSlider?.setValue(Float(seconds), animated: true)
+            originalTimestamp?.text = "\(seconds.timeString())"
+        case .skipped:
+            guard let seconds = skippedPlayer?.currentTime else { return }
+            skippedSlider?.setValue(Float(seconds), animated: true)
+            skippedTimestamp?.text = "\(seconds.timeString())"
+        }
+    }
+}
+
+extension AudioViewController: AVAudioPlayerDelegate {
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        if player == skippedPlayer {
+            skippedPlayer = nil
+            skippedPlayPauseButton?.setImage(playImage, for: .normal)
+        } else if player == originalPlayer {
+            originalPlayer = nil
+            originalPlayPauseButton?.setImage(playImage, for: .normal)
+        }
+    }
+}
+
+
+extension Double {
+    func roundTo(places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
 }
 
 extension TimeInterval {
@@ -252,13 +241,5 @@ extension TimeInterval {
     }
 }
 
-
-extension Double {
-    /// Rounds the double to decimal places value
-    func roundTo(places:Int) -> Double {
-        let divisor = pow(10.0, Double(places))
-        return (self * divisor).rounded() / divisor
-    }
-}
 
 
